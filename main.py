51f9034f89a4db5c4fd1ae1e97e3a91241250ef7
sarpi <a_sarpi@mail.ru>
2024-07-08 22:02:25 +0300

    at first just a plot

--- main.py
+++ main.py
@@ -1,51 +1,27 @@
+from PyQt6.QtWidgets import QWidget, QApplication, QVBoxLayout
+from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
 import sys
-from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtCore import Qt
 
 
-class TableModel(QtCore.QAbstractTableModel):
-    def __init__(self, data):
-        super(TableModel, self).__init__()
-        self._data = data
-
-    def data(self, index, role):
-        if role == Qt.DisplayRole:
-            # See below for the nested-list data structure.
-            # .row() indexes into the outer list,
-            # .column() indexes into the sub-list
-            return self._data[index.row()][index.column()]
-
-    def rowCount(self, index):
-        # The length of the outer list.
-        return len(self._data)
-
-    def columnCount(self, index):
-        # The following takes the first sub-list, and returns
-        # the length (only works if all rows are an equal length)
-        return len(self._data[0])
-
-
-class MainWindow(QtWidgets.QMainWindow):
+class MyWindow(QWidget):
     def __init__(self):
         super().__init__()
+        layout = QVBoxLayout()
+        self.setLayout(layout)
 
-        self.table = QtWidgets.QTableView()
-
-        data = [
-          [4, 9, 2],
-          [1, 0, 0],
-          [3, 5, 0],
-          [3, 3, 2],
-          [7, 8, 9],
-        ]
-
-        self.model = TableModel(data)
-        self.table.setModel(self.model)
-
-        self.setCentralWidget(self.table)
+        canvas = FigureCanvas(fig)  # create canvas
+        layout.addWidget(canvas)  # add canvas to layout
 
 
-app=QtWidgets.QApplication(sys.argv)
-window=MainWindow()
+fig = Figure()
+ax = fig.add_subplot()
+
+x = [1, 2, 3, 4, 5]
+y = [1, 4, 9, 16, 25]
+ax.plot(x, y)
+
+app = QApplication(sys.argv)
+window = MyWindow()
 window.show()
-app.exec_()
+sys.exit(app.exec())
